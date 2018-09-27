1391c64640d096a1d23a0c5a12496eade3de70c9
Work <work@Jims-MacBook-Pro.local>
2018-09-27 14:55:29 -0700

    revise and slightly prettify dsf_info.py

--- dsf_info.py
+++ dsf_info.py
@@ -1,1 +1,5 @@
-print("https://github.com/knathanieltucker/data-science-foundations")
+print("\n Some important links: \n \n"
+	"Nate Tucker's DSF Curriculum Page: \n"
+	"https://github.com/knathanieltucker/data-science-foundations \n \n"
+	"General Assembly's Course Info Page: \n" 
+	"https://git.generalassemb.ly/nate/course-info/tree/master")
