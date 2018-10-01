d6902ce1281deac36a5e1a38e5f0bdcdbafbdb64
Work <work@Jims-MacBook-Pro.local>
2018-09-30 23:09:51 -0700

    Fix file structure

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dsfinfo",
-    version="0.0.1",
+    version="0.0.2",
     author="Jim Knudstrup",
     author_email="jim.knudstrup@gmail.com",
     description="Some Data Science Foundations links.",
